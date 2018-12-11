05f533e1831208a56727e35781e42de7314337bb
Oleg L <oleg.l@smatym.pro>
2018-12-11 13:38:01 +0600

    Initial Commit

---
+++ KaragandyBusTests/KaragandyBusTests.swift
@@ -0,0 +1,34 @@
+//
+//  KaragandyBusTests.swift
+//  KaragandyBusTests
+//
+//  Created by Олег Лейзер on 11/12/2018.
+//  Copyright © 2018 Олег Лейзер. All rights reserved.
+//
+
+import XCTest
+@testable import KaragandyBus
+
+class KaragandyBusTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
